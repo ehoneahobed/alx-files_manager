f0827023fd5f6c608104966262d7b8390202966f
ehoneahobed <ehoneahobed@hotmail.com>
2023-06-01 00:41:51 +0000

    Add solution to task 0

---
+++ README.md
@@ -0,0 +1,1 @@
+# Solutions to tasks on 0x04. Files manager
---
+++ utils/redis.js
@@ -0,0 +1,39 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        this.client.on('error', (error) => {
+            console.log(error);
+        });
+        this.connected = false;
+        this.client.on('connect', () => {
+            this.connected = true;
+        })
+    }
+
+    isAlive() {
+        return this.connected;
+    }
+
+    async get(key) {
+        const result = promisify(this.client.get).bind(this.client);
+        const value = await result(key);
+        return value;
+    }
+
+    async set(key, value, duration) {
+        this.client.setex(key, duration, value);
+    }
+
+    async del(key) {
+        this.client.del(key);
+    }
+
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
